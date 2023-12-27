bcc50e5f9531f6f293fce5c31f6a88ea7038b5d3
shameer kharel <126055775+sameer-kharel@users.noreply.github.com>
2023-12-27 08:42:04 +0545

    Updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,68 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+def main():
+    st.title("Machine Learning and Artificial Intelligence")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    st.write("Welcome to the interactive blog post about Machine Learning and Artificial Intelligence!")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    st.header("Machine Learning (ML)")
+    st.write("Machine Learning is a subset of Artificial Intelligence that focuses on developing algorithms and models "
+             "that enable computers to learn from data and make predictions or decisions without being explicitly programmed.")
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+    st.subheader("Common ML Techniques:")
+    st.markdown("- Supervised Learning: Models learn from labeled training data.")
+    st.markdown("- Unsupervised Learning: Models find patterns in unlabeled data.")
+    st.markdown("- Reinforcement Learning: Agents learn by interacting with an environment and receiving feedback.")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    st.beta_expander("Supervised Learning in Detail"):
+        st.write("Supervised Learning involves training a model on a labeled dataset, where each example is paired with a "
+                 "correct label. The model makes predictions on new, unseen data.")
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+        st.subheader("Example Algorithms:")
+        st.markdown("- Linear Regression")
+        st.markdown("- Decision Trees")
+        st.markdown("- Support Vector Machines")
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+    st.beta_expander("Unsupervised Learning in Detail"):
+        st.write("Unsupervised Learning deals with finding patterns and relationships in data without labeled outcomes. "
+                 "Clustering and dimensionality reduction are common tasks.")
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+        st.subheader("Example Algorithms:")
+        st.markdown("- K-Means Clustering")
+        st.markdown("- Principal Component Analysis (PCA)")
+        st.markdown("- Autoencoders")
+
+    st.beta_expander("Reinforcement Learning in Detail"):
+        st.write("Reinforcement Learning involves an agent learning to make decisions by interacting with an environment. "
+                 "The agent receives feedback in the form of rewards or penalties.")
+
+        st.subheader("Example Algorithms:")
+        st.markdown("- Q-Learning")
+        st.markdown("- Deep Q Networks (DQN)")
+        st.markdown("- Policy Gradient Methods")
+
+    st.header("Artificial Intelligence (AI)")
+    st.write("Artificial Intelligence is a broader concept that encompasses machines or systems that can perform tasks "
+             "that typically require human intelligence. Machine Learning is a subset of AI.")
+
+    st.subheader("AI Applications:")
+    st.markdown("- Natural Language Processing (NLP)")
+    st.markdown("- Computer Vision")
+    st.markdown("- Speech Recognition")
+    st.markdown("- Robotics")
+
+    st.header("Interactive Demo")
+
+    st.write("Let's explore a simple interactive demo to understand your preferences and experiences in ML and AI.")
+
+    ml_experience = st.slider("How familiar are you with Machine Learning?", 0, 10, 5)
+    ai_experience = st.slider("How familiar are you with Artificial Intelligence?", 0, 10, 3)
+
+    st.write(f"Your Machine Learning experience level: {ml_experience}")
+    st.write(f"Your Artificial Intelligence experience level: {ai_experience}")
+
+    st.success("Thank you for exploring the interactive demo! Feel free to reach out if you have any questions.")
+
+if __name__ == "__main__":
+    main()
+
